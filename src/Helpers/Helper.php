cad21da84d514341d012150070c8fec4775f6b4c
Ben Hirst <ben@Bens-MacBook-Pro.local>
2022-05-16 14:02:14 +0100

    add helpers

---
+++ src/Helpers/Helper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Skeleton\Helpers;
+
+abstract class Helper
+{
+    final public function __construct(protected array $options)
+    {
+        //
+    }
+
+    final public static function create(array $options)
+    {
+        $instance = new static($options);
+        return $instance;
+    }
+}
