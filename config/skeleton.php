0698e02a95d8f8927d1796cccf99b80fe2376ac2
Ben Hirst <ben@Bens-MacBook-Pro.local>
2022-05-16 12:21:40 +0100

    test

--- config/skeleton.php
+++ config/skeleton.php
@@ -4,8 +4,28 @@ use Benhdev\Skeleton\Skeleton;
 
 return [
 
-    'guards' => [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Guards
+    |--------------------------------------------------------------------------
+    |
+    | Custom authentication guard mappings for the application
+    |
+    | To create a new guard you can run
+    | php artisan make:guard ExampleGuard
+    |
+    | You can then use this guard in route definitions
+    | using the guard name as below
+    |
+    | Route::middleware('auth:custom')
+    |
+    */
 
+    'guards' => [
+        'custom' => [
+            'driver' => \App\Guards\ExampleGuard::class,
+            'provider' => 'users'
+        ],
     ],
-    
+
 ];
--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -3,7 +3,11 @@
 namespace Benhdev\Skeleton;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+
 use Illuminate\Support\Facades\Log;
+
+use ReflectionClass;
 
 class SkeletonServiceProvider extends ServiceProvider
 {
@@ -29,5 +33,34 @@ class SkeletonServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/skeleton.php' => config_path('skeleton.php')
         ]);
+
+        foreach ($this->guards() as $name => $config) {
+            if (!class_exists($config['driver'])) {
+                continue;
+            }
+
+            $reflectionDriver = new ReflectionClass($config['driver']);
+            $driver = Str::slug($reflectionDriver->getShortName());
+
+            Log::debug($driver);
+
+            config([
+                "auth.guards.{$name}" => array_merge([
+                    'driver' => $driver,
+                    'provider' => $config['provider'] ?? null,
+                ], config("auth.guards.{$name}", [])),
+            ]);
+
+            $this->app->auth->extend($driver, function ($app, $name, array $config) use ($reflectionDriver) {
+                return $reflectionDriver->newInstance(
+                    $app->auth->createUserProvider($config['provider'] ?? null), $app->request
+                );
+            });
+        }
+    }
+
+    protected function guards()
+    {
+        return config('skeleton.guards');
     }
 }
